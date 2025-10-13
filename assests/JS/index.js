d5a8446564a87219cdb2b1e5d2de9b0ea02985c5
Gopinath <162040802+Gopinathhh@users.noreply.github.com>
2025-10-14 00:47:52 +0530

    Add files via upload

---
+++ assests/JS/index.js
@@ -0,0 +1,48 @@
+
+      const menuToggle = document.querySelector('.menu-toggle');
+      const navbar = document.querySelector('.navbar');
+
+      menuToggle.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+      });
+
+       const moonIcon = document.querySelector('.moon-icon');
+      moonIcon.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+          moonIcon.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        } else {
+          moonIcon.innerHTML = '<i class="fa-solid fa-moon"></i>';
+        }
+      });
+
+      /*
+      const textTarget = document.querySelector('.text-target');
+      const roles = ['Dynamic', 'designing', 'developing'];
+      let roleIndex = 0;
+      let charIndex = 0;
+
+      function typeRole() {
+        if (charIndex < roles[roleIndex].length) {
+          textTarget.textContent += roles[roleIndex].charAt(charIndex);
+          charIndex++;
+          setTimeout(typeRole, 150);
+        } else {
+          setTimeout(eraseRole, 2000);
+        }
+      }
+
+      function eraseRole() {
+        if (charIndex > 0) {
+          textTarget.textContent = roles[roleIndex].substring(0, charIndex - 1);
+          charIndex--;
+          setTimeout(eraseRole, 100);
+        } else {
+          roleIndex = (roleIndex + 1) % roles.length;
+          setTimeout(typeRole, 500);
+        }
+      }
+
+      document.addEventListener('DOMContentLoaded', () => {
+        if (roles.length) setTimeout(typeRole, 500);
+      });*/
